0748ff012c24b2c0bad2c59e753316ace36c0881
John Carlos Soldevilla <soldevillajohncarlos@gmail.com>
2025-10-31 02:50:57 +0800

    Setup clerk and convex

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "html.tailus.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
